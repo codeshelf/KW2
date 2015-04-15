06d69746804805d7b1fe1b6cc8a8ef3e49fffae8
Jeff Williams <jeff.williams@codeshelf.com>
2015-04-15 16:53:00 -0700

    * Commit tuner task stuff.

---
+++ Application/Configure/tunerTask.h
@@ -0,0 +1,20 @@
+/*
+ FlyWeight
+ © Copyright 2005, 2006 Jeffrey B. Williams
+ All rights reserved
+
+ $Id$
+ $Name$
+ */
+
+#ifndef TUNERTASK_H
+#define TUNERTASK_H
+
+#include "FreeRTOS.h"
+
+// --------------------------------------------------------------------------
+// Local functions prototypes.
+
+void tunerTask(void *pvParameters);
+
+#endif TUNERTASK_H
---
+++ Application/Source/tunerTask.c
@@ -0,0 +1,91 @@
+/*
+ FlyWeight
+ © Copyright 2005, 2006 Jeffrey B. Williams
+ All rights reserved
+
+ $Id$
+ $Name$
+ */
+
+#include "tunerTask.h"
+#include "SMAC.h"
+#include "task.h"
+#include "queue.h"
+#include "commands.h"
+#include "Wait.h"
+
+#define DEFAULT_CRYSTAL_TRIM	0xf4
+#define PERFECT_REMAINDER		0x6b06
+
+xTaskHandle gTunerTask = NULL;
+
+void adjustTune(gwUINT8 trim);
+gwUINT16 measureTune(void);
+
+extern ELocalStatusType gLocalDeviceState;
+
+// --------------------------------------------------------------------------
+
+void tunerTask(void *pvParameters) {
+
+	gwUINT8 ccrHolder;
+	gwUINT8 trim;
+	gwUINT16 remainder;
+	
+//	for (;;) {
+		GW_ENTER_CRITICAL(ccrHolder);
+		
+		trim = DEFAULT_CRYSTAL_TRIM;
+		adjustTune(trim);
+		remainder = measureTune();
+		if (remainder > PERFECT_REMAINDER) {
+			while ((remainder > PERFECT_REMAINDER) && (trim < 255)) {
+				adjustTune(++trim);
+				remainder = measureTune();
+			}
+		} else if (remainder < PERFECT_REMAINDER) {
+			while ((remainder < PERFECT_REMAINDER) && (trim > 0)) {
+				adjustTune(--trim);
+				remainder = measureTune();
+			}
+		}
+		GW_EXIT_CRITICAL(ccrHolder);
+		
+		Wait_Waitms(8500);
+		Wait_Waitms(1);
+//	}
+}
+
+void adjustTune(gwUINT8 trim) {
+	MLMEXtalAdjust(trim); 
+	// Wait for the clocks to stabilize.
+	Wait_Waitms(200);
+}
+
+gwUINT16 measureTune() {
+	
+	gwUINT16 remainder = 0;
+	
+	// Capture and measure the rising edges of the GPS PPS signal.
+	Tuner_Init();
+	FTM0_SC = 0x80;
+	FTM0_C2V = 0;
+	FTM0_C3V = 0;
+	FTM0_MOD = 0xffff;
+	FTM0_CNTIN = 0x0001;
+	FTM0_CNT = 0x0000;
+	FTM0_SC = 0x88;
+	FTM0_COMBINE |= (FTM_COMBINE_DECAP1_MASK);
+	// Loop until there is an event.
+	while ((FTM0_STATUS & FTM_STATUS_CH3F_MASK) == 0) {
+		GW_WATCHDOG_RESET;
+	}
+
+	if (FTM0_C2V < FTM0_C3V) {
+		remainder = FTM0_C3V - FTM0_C2V;
+	} else {
+		remainder = (0xffff - FTM0_C2V) + FTM0_C3V;
+	}
+	
+	return remainder;
+}
