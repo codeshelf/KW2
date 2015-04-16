25f8b844bf6f364e41ab5c86abcfc6fe595070e8
Andrew R. Huff <areederhuff@gmail.com>
2015-04-16 10:33:44 -0700

    Added eeprom functionality

--- Application/Configure/eeprom.h
+++ Application/Configure/eeprom.h
@@ -11,6 +11,7 @@
 #include "PE_Types.h"
 #include "EepromCS.h"
 #include "Wait.h"
+#include "SPI_PDD.h"
 
 #define Eeprom_DEVICE 		SPI0_BASE_PTR
 
@@ -19,13 +20,38 @@
 #define READSRS_CMD			0b00000101
 #define WRITESRS_CMD		0b00000001
 #define WRITE_ENABLE_CMD	0b00000110
-#define WRITE_DISABLECMD	0b00000100
+#define WRITE_DISABLE_CMD	0b00000100
 
-#define EEPROM_CS_ON     	EepromCS_ClrVal();
+// Read 1 byte from RX FIFO and clear RXFIFO to fix RXFIFO pointer error
+#define EEPROM_CS_ON     	EepromCS_ClrVal(); SPI_PDD_ReadData8bits(Eeprom_DEVICE); SPI_PDD_ClearRxFIFO(Eeprom_DEVICE);
 #define EEPROM_CS_OFF    	Wait_Waitus(5); EepromCS_SetVal();
+
+#define EEPROM_PAGE_SIZE	32			// bytes
+#define EEPROM_TWC			50000000	// ns
+#define SPI_READ_DELAY		10000000	// ns
+
+// Length of values stored in EEPROM in bytes
+#define EEPROM_AES_KEY_LEN		16
+#define EEPROM_GUID_LEN			8
+#define EEPROM_HW_VER_LEN		8
+#define EEPROM_TUNING_LEN		1
+
+// EEPROM memory locations
+#define EEPROM_AES_KEY_ADDR		0
+#define EEPROM_GUID_ADDR		EEPROM_AES_KEY_ADDR + EEPROM_AES_KEY_LEN
+#define EEPROM_HW_VER_ADDR		EEPROM_GUID_ADDR + EEPROM_GUID_LEN
+#define EEPROM_TUNING_ADDR		EEPROM_HW_VER_ADDR + EEPROM_HW_VER_LEN
 
 void spiSendByte(uint8_t data);
 uint8_t spiGetByte(void);
-bool readEepromData(uint16_t addr, uint8_t* dataPtr, uint8_t bytesToRead);
+void readEepromData(uint16_t addr, uint8_t* dataPtr, uint8_t bytesToRead);
+void writeEepromData(uint16_t addr, uint8_t* dataPtr, uint8_t bytesToWrite);
+
+void writeAESKey(uint8_t* dataPtr);
+void readAESKey(uint8_t* dataPtr);
+void writeHWVersion(uint8_t* dataPtr);
+void readHWVersion(uint8_t* dataPtr);
+void writeGuid(uint8_t* dataPtr);
+void readGuid(uint8_t* dataPtr);
 
 #endif /* EEPROM_H_ */
--- Application/Source/eeprom.c
+++ Application/Source/eeprom.c
@@ -2,61 +2,124 @@
 #include "PE_Types.h"
 #include "IO_Map.h"
 #include "string.h"
-//#include "SharpDisplay.h"
 #include "SPI_PDD.h"
+#include "commands.h"
 
+#define WAIT_SPI0_TRANSMISSON_END(count) {while ((SPI0_SR & SPI_SR_RXCTR_MASK) < (count<<SPI_SR_RXCTR_SHIFT)) {};}
+
+/*
+ * Will get one byte from the eeprom.
+ * 
+ * Assumes pattern: request one byte, read one byte, request one byte...
+ * 
+ * If a different pattern is desired you'll have to move the WAIT_SPI0_TRANSMISSION_END(n)
+ * out and use it to wait for incoming data like the following:
+ * 
+ * spiSendByte(data)
+ * spiSendByte(data)
+ * spiSendByte(data)
+ * WAIT_SPI0_TRANSMISSON_END(3);
+ * rcv1 = spiGetByte();
+ * rcv2 = spiGetByte();
+ * rcv3 = spiGetByte();
+ */
 void spiSendByte(uint8_t data) {
-
 	// Wait until TX buffer empty.
 	while (SPI_PDD_GetTxFIFOCounter(Eeprom_DEVICE) > 1)
 		;
 	SPI_PDD_WriteData8Bits(Eeprom_DEVICE, data);
-	Wait_Waitns(700);
-
+	
+	WAIT_SPI0_TRANSMISSON_END(1);
 }
 
 uint8_t spiGetByte() {
-	uint8_t result = 0;
+	uint8_t result;
 
 	// Wait until RX buffer not empty.
 	while (SPI_PDD_GetRxFIFOCounter(Eeprom_DEVICE) == 0)
 		;
 	result = SPI_PDD_ReadData8bits(Eeprom_DEVICE);
-	Wait_Waitns(700);
 
 	return result;
 }
 
-bool readEepromData(uint16_t addr, uint8_t* dataPtr, uint8_t bytesToRead) {
-	bool result = FALSE;
+/*
+ * Will read as many bytes as requested. Does not loop on page end.
+ * 
+ */
+void readEepromData(uint16_t addr, uint8_t* dataPtr, uint8_t bytesToRead) {
+	gwUINT8 ccrHolder;
+	
+	GW_ENTER_CRITICAL(ccrHolder);
+	
+	EEPROM_CS_ON
+	
+	spiSendByte(READ_CMD);
+	spiGetByte();
+	spiSendByte(addr >> 8);
+	spiGetByte();
+	spiSendByte( addr & 0xFF);
+	spiGetByte();
+	
+	for(uint8_t i = 0; i < bytesToRead; i++) {
+		spiSendByte(0xff);
+		dataPtr[i] = spiGetByte();
+	}
+	
+	EEPROM_CS_OFF
+	GW_EXIT_CRITICAL(ccrHolder);
+}
 
-//	EEPROM_CS_ON
-//
-//	spiSendByte(WRITE_ENABLE_CMD);
-//
-//	EEPROM_CS_OFF
-//	EEPROM_CS_ON
-//
-//	spiSendByte(WRITE_CMD);
-//	spiSendByte(addr >> 8);
-//	spiSendByte(addr & 0xff);
-//	spiSendByte(0xaa);
-//
-//	EEPROM_CS_OFF
+
+/*
+ * Need to be careful not to write beyond page. Will loop and overwrite
+ * data in current page.
+ * 
+ * Physical page boundaries start at addresses that are integer multiples 
+ * of the page buffer size (32 bytes) (or ‘page size’) and, end at 
+ * addresses that are integer multiples of page size – 1
+ */
+void writeEepromData(uint16_t addr, uint8_t* dataPtr, uint8_t bytesToWrite) {
+	
+	EEPROM_CS_ON
+	spiSendByte(WRITE_ENABLE_CMD);
+	EEPROM_CS_OFF
 	EEPROM_CS_ON
 
-	spiSendByte(READ_CMD);
+	spiSendByte(WRITE_CMD);
 	spiSendByte(addr >> 8);
-	spiSendByte(addr & 0xFF);
-	spiSendByte(0xff);
-	spiSendByte(0xff);
-	spiSendByte(0xff);
-	spiSendByte(0xff);
-	spiSendByte(0xff);
+	spiSendByte(addr & 0xff);
 	
-	dataPtr[0] = spiGetByte();
+	for(uint8_t i = 0; i < bytesToWrite; i++) {
+		spiSendByte(dataPtr[i]);
+	}
 	
 	EEPROM_CS_OFF
 	
-	return result;
+	// Wait for internal write cycle time (TWC)
+	Wait_Waitns(EEPROM_TWC);
+}
+
+void writeAESKey(uint8_t* dataPtr) {
+	writeEepromData((uint16_t)EEPROM_AES_KEY_ADDR, dataPtr, (uint8_t)EEPROM_AES_KEY_LEN);
+}
+
+void readAESKey(uint8_t* dataPtr) {
+	readEepromData((uint16_t)EEPROM_AES_KEY_ADDR, dataPtr, (uint8_t)EEPROM_AES_KEY_LEN);
+}
+
+void writeHWVersion(uint8_t* dataPtr) {
+	writeEepromData((uint16_t)EEPROM_HW_VER_ADDR, dataPtr, (uint8_t)EEPROM_HW_VER_LEN);
+}
+
+void readHWVersion(uint8_t* dataPtr) {
+	readEepromData((uint16_t)EEPROM_HW_VER_ADDR, dataPtr, (uint8_t)EEPROM_HW_VER_LEN);
+}
+
+void writeGuid(uint8_t* dataPtr) {
+	writeEepromData((uint16_t)EEPROM_GUID_ADDR, dataPtr, (uint8_t)EEPROM_GUID_LEN);
+}
+
+void readGuid(uint8_t* dataPtr) {
+	readEepromData((uint16_t)EEPROM_GUID_ADDR, dataPtr, (uint8_t)EEPROM_GUID_LEN);
 }
